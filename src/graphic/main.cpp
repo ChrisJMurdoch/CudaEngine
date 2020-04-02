396a1305243145867e5ad1a220e8d4caba991312
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2020-04-02 12:58:56 +0100

    Cleaned up and refactored graphic main

--- src/graphic/main.cpp
+++ src/graphic/main.cpp
@@ -2,101 +2,71 @@
 #include <string>
 #include <fstream>
 #include <sstream>
-#include <iostream>
 
-#include <stdio.h>
-#include <stdlib.h>
-
-// Include GLEW
 #include <GL/glew.h>
-
-// Include GLFW
+#include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
 GLFWwindow* window;
 
 #include "..\..\include\graphic\main.hpp"
-
-// Mostly triangle tutorial code for now, from http://www.opengl-tutorial.org
+#include "..\..\include\logger\log.hpp"
 
 int main( void )
 {
 	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
-		return -1;
-	}
+	glfwInit();
 
+	// Create window
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// Open a window and create its OpenGL context
 	window = glfwCreateWindow( 1024, 768, "CudaEngine", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\n" );
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
 	glfwMakeContextCurrent(window);
 
 	// Initialize GLEW
-	glewExperimental = true; // Needed for core profile
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
+	glewExperimental = true;
+	glewInit();
 
-	// Capture the escape key being pressed below
+	// Setup key press mode
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
 
 	// Create and compile GLSL program from the shaders
 	GLuint programID = LoadShaders( "shaders\\SimpleVertexShader.vert", "shaders\\SimpleFragmentShader.frag" );
 	if ( programID == 0 ) return 1;
 
+	// DRAW
+
+	// Dark blue background
+	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 
 	static const GLfloat g_vertex_buffer_data[] = { 
 		-1.0f, -1.0f, 0.0f,
 		 1.0f, -1.0f, 0.0f,
 		 0.0f,  1.0f, 0.0f,
 	};
-
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	do{
-
-		// Clear the screen
+	do
+	{
+		// Clear screen
 		glClear( GL_COLOR_BUFFER_BIT );
 
-		// Use our shader
+		// Use program shaders
 		glUseProgram(programID);
 
-		// 1rst attribute buffer : vertices
+		// 1st attribute buffer : vertices
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
+		glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0 );
 
 		// Draw the triangle
 		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
@@ -106,17 +76,14 @@ int main( void )
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+	}
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
 
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
-
-	// Cleanup VBO
+	// Cleanup
 	glDeleteBuffers(1, &vertexbuffer);
 	glDeleteVertexArrays(1, &VertexArrayID);
 	glDeleteProgram(programID);
 
-	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
 
 	return 0;
@@ -155,7 +122,7 @@ GLuint LoadShaders(const char *vertFilePath,const char *fragFilePath)
 	glGetShaderiv(fragShaderID, GL_COMPILE_STATUS, &fragSucc);
 	if (!vertSucc || !fragSucc)
 	{
-		std::cout << "Shader compilation error." << std::endl;
+		Log::print(Log::error, "Shader compilation error.");
 		return 0;
 	}
 
@@ -170,7 +137,7 @@ GLuint LoadShaders(const char *vertFilePath,const char *fragFilePath)
 	glGetProgramiv(programID, GL_LINK_STATUS, &progSucc);
 	if (!progSucc)
 	{
-		std::cout << "Shader linking error." << std::endl;
+		Log::print(Log::error, "Shader linking error.");
 		return 0;
 	}
 	

63b320ad42e785ce837f7879b34459fefc3a427b
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2020-04-03 06:57:07 +0100

    Implemented EBO rendering

--- src/graphic/main.cpp
+++ src/graphic/main.cpp
@@ -64,33 +64,46 @@ int main()
 		return -1;
 	}
 
-	// Create triangle coords
+	// Create square coords
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
+		0.5f,  0.5f, 0.0f,
 		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
-	};  
+		-0.5f, -0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f 
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	}; 
 
 	// Create buffer and array objects
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	// Bind objects
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
 	// Copy over buffer data
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Give OpenGL vertex buffer format
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// Unbind buffer and array
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// Unbind buffers and array
 	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	
+	// Use wireframe mode
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
 	while( !glfwWindowShouldClose(window) )
 	{
 		// Get key presses
@@ -102,11 +115,14 @@ int main()
 		// Use program shaders
 		glUseProgram(programID);
 
-		// Use VAO
+		// Bind VAO
 		glBindVertexArray(VAO);
 
 		// Draw the triangle
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		// Unbind VAO
+		glBindVertexArray(0);
 
 		// Check inputs and display
 		glfwSwapBuffers(window);

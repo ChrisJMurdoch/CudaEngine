d0404e4ae5bfaee21e997671b4395395e39beee2
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2020-04-03 04:39:19 +0100

    Moved from GLEW to GLAD

--- include/graphic/main.hpp
+++ include/graphic/main.hpp
@@ -1,4 +1,6 @@
 
 #pragma once
 
+void processInput(GLFWwindow *window);
+void resizeCallback(GLFWwindow *window, int width, int height);
 GLuint LoadShaders(const char *vertFilePath,const char *fragFilePath);
--- src/graphic/main.cpp
+++ src/graphic/main.cpp
@@ -3,43 +3,67 @@
 #include <fstream>
 #include <sstream>
 
-#include <GL/glew.h>
-#include <glm/glm.hpp>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 GLFWwindow* window;
 
 #include "..\..\include\graphic\main.hpp"
 #include "..\..\include\logger\log.hpp"
 
-int main( void )
+#define VIEW_WIDTH 800
+#define VIEW_HEIGHT 600
+
+int main()
 {
 	// Initialise GLFW
 	glfwInit();
 
-	// Create window
+	// Supersample
 	glfwWindowHint(GLFW_SAMPLES, 4);
+	// OpenGL Version
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Force modern GL
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	window = glfwCreateWindow( 1024, 768, "CudaEngine", NULL, NULL);
+	// Mac compatibility
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	// Create window
+	window = glfwCreateWindow( VIEW_WIDTH, VIEW_HEIGHT, "CudaEngine", NULL, NULL);
+	if (window == NULL)
+	{
+		Log::print(Log::error, "GLFW window creation error.");
+		glfwTerminate();
+		return -1;
+	}
 	glfwMakeContextCurrent(window);
 
-	// Initialize GLEW
-	glewExperimental = true;
-	glewInit();
+	// Initialize GLAD
+	if ( !gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) )
+	{
+		Log::print(Log::error, "GLAD loading error.");
+		glfwTerminate();
+		return -1;
+	}
+
+	// Set bounds
+	glViewport(0, 0, VIEW_WIDTH, VIEW_HEIGHT);
+	// Allow resize to window
+	glfwSetFramebufferSizeCallback(window, resizeCallback);
 
 	// Setup key press mode
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
+	/*
 	// Create and compile GLSL program from the shaders
 	GLuint programID = LoadShaders( "shaders\\SimpleVertexShader.vert", "shaders\\SimpleFragmentShader.frag" );
 	if ( programID == 0 ) return 1;
 
 	// DRAW
-
+	
+	*/
 	// Dark blue background
 	glClearColor(0.5f, 0.5f, 0.5f, 0.0f);
-
+	/*
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
@@ -54,12 +78,16 @@ int main( void )
 		 0.0f,  1.0f, 0.0f,
 	};
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-	do
+	*/
+	while( !glfwWindowShouldClose(window) )
 	{
+		// Get key presses
+		processInput(window);
+
 		// Clear screen
 		glClear( GL_COLOR_BUFFER_BIT );
 
+		/*
 		// Use program shaders
 		glUseProgram(programID);
 
@@ -71,22 +99,32 @@ int main( void )
 		// Draw the triangle
 		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
 
-		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(0);*/
 
-		// Swap buffers
-		glfwSwapBuffers(window);
+		// Check inputs and display
 		glfwPollEvents();
+		glfwSwapBuffers(window);
 	}
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
 
 	// Cleanup
-	glDeleteBuffers(1, &vertexbuffer);
+	/*glDeleteBuffers(1, &vertexbuffer);
 	glDeleteVertexArrays(1, &VertexArrayID);
-	glDeleteProgram(programID);
+	glDeleteProgram(programID);*/
 
 	glfwTerminate();
 
 	return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void resizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
 
 GLuint LoadShaders(const char *vertFilePath,const char *fragFilePath)

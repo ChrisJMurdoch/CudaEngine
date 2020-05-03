3dca6faf1fdbf726d7911f1af36f7c54e8553021
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2020-05-03 02:37:50 +0100

    Altered Log header

--- include/logger/log.hpp
+++ include/logger/log.hpp
@@ -1,21 +1,19 @@
 
-
 #pragma once
 
 class Log
 {
 public:
+    enum Level { debug=0, message=1, warning=2, error=3, force=4 };
     static const bool NEWLINE = true;
     static const bool NO_NEWLINE = false;
-    enum Level { debug=0, message=1, warning=2, error=3, force=4 };
-
-    static void set(Level lvl);
-
-    template <class T>
-    static void print(Level lvl, T message, bool newline=true);
-
-    static void check(int result, const char *op);
 
 private:
     static Level level;
+
+public:
+    static void set(Level lvl);
+    template <class T>
+    static void print(Level lvl, T message, bool newline=true);
+    static void check(int result, const char *op);
 };
